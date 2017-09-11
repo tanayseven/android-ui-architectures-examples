4610caebbf047ef2bf58ed6e8bdedb66cd65bdc0
Tanay PrabhuDesai <tanayseven@gmail.com>
2017-09-11 12:18:17 +0530

    Add layout for Rx Weather screen

--- android-app/app/src/main/java/com/tanayseven/uiarchitectures/MainActivity.java
+++ android-app/app/src/main/java/com/tanayseven/uiarchitectures/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tanayseven.uiarchitectures;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tanayseven.uiarchitectures.rxWeather.RxWeatherFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadHomeFragment();
+    }
+
+    private void loadHomeFragment() {
+        RxWeatherFragment weatherRxFragment = new RxWeatherFragment();
+        this.switchFragment(weatherRxFragment, RxWeatherFragment.TAG);
+    }
+
+    public void switchFragment(Fragment fragment, String tag) {
+        getFragmentManager().beginTransaction().replace(R.id.main_activity_container, fragment, tag);
     }
 }
---
+++ android-app/app/src/main/java/com/tanayseven/uiarchitectures/rxWeather/RxWeatherFragment.java
@@ -0,0 +1,12 @@
+package com.tanayseven.uiarchitectures.rxWeather;
+
+import android.app.Fragment;
+
+/**
+ * Created by tanay on 11/9/17.
+ */
+
+public class RxWeatherFragment extends Fragment {
+    public static final String TAG = RxWeatherFragment.class.getSimpleName();
+
+}
